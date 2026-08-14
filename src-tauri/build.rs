b27becea7c32a74286ee6559ceea42adee810b0d
shree <2851681799@qq.com>
2026-08-14 11:35:59 +0800

    feat: refresh application icons

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=app.manifest");
+    println!("cargo:rerun-if-changed=icons");
     let windows = tauri_build::WindowsAttributes::new().app_manifest(include_str!("app.manifest"));
     let attributes = tauri_build::Attributes::new().windows_attributes(windows);
     tauri_build::try_build(attributes).expect("failed to run tauri build script");
